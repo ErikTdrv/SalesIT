cc88bce5a1170fd510c19b43f2917f7a79b56556
Erik Todorov <eriktdrv@gmail.com>
2023-02-21 23:44:51 +0200

    Adding routes

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-
+const authController = require('./controllers/authController')
     router.get('/', (req, res) => {
         console.log('React API working!')
         res.end()
     })
-    
+    router.use(authController)
 module.exports = router;
