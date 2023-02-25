69e376268e7abfe8d669bf1cf026d59348bd5f43
Erik Todorov <eriktdrv@gmail.com>
2023-02-25 23:19:04 +0200

    Adding login path

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
