eeaa036d4798a5195700f5524f1de41b30094c85
Erik Todorov <eriktdrv@gmail.com>
2023-02-25 22:24:41 +0200

    Adding JSX & CSS to Home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,24 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
-export default function Home(){
-    return ();
+export default function Home() {
+  return (
+    <>
+    <div class="home">
+      <img src="../../home-page.jpg" alt="" />
+      <div class="titles">
+        <h1 class="first">The Ultimate Destination for Tech Lovers</h1>
+        <h2 class="second">
+          From laptops to accessories, we have everything you need to stay on
+          top of your tech game - shop with us today!
+        </h2>
+        <div class="home-buttons">
+          <button>Login</button>
+          <button>Register</button>
+          <button>All Products</button>
+        </div>
+      </div>
+    </div>
+    </>
+  );
 }
