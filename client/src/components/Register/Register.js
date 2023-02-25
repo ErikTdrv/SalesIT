e5b35fa2018eb731b410a6b06fe3664f0d732a6c
Erik Todorov <eriktdrv@gmail.com>
2023-02-26 00:06:38 +0200

    Adding classes + using simple state

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -4,6 +4,34 @@ import Copyright from "../Copyright/Copyright";
 import "./Register.css";
 
 export default function Register() {
+  const [username, setUsername] = useState(false);
+  const [password, setPassword] = useState(false);
+  const [email, setEmail] = useState(false);
+  const [repass, setRepass] = useState(false);
+
+  function changeValue(e){
+    if(e.target.value.length > 0){
+        if(e.target.className === 'first'){
+            setUsername(true)
+        }else if(e.target.className === 'second'){
+            setEmail(true)
+        }else if(e.target.className === 'third'){
+            setPassword(true)
+        }else if(e.target.className === 'fourth'){
+            setRepass(true)
+        }
+    }else {
+        if(e.target.className === 'first'){
+            setUsername(false)
+        }else if(e.target.className === 'second'){
+            setEmail(false)
+        }else if(e.target.className === 'third'){
+            setPassword(false)
+        }else if(e.target.className === 'fourth'){
+            setRepass(false)
+        }
+    }
+  }
   return (
     <>
       <div className="register">
@@ -16,20 +44,20 @@ export default function Register() {
           />
           <div className="register-inputs">
             <div className="register-username">
-              <input type="text" />
-              <span className="reg-username-span">Username</span>
+              <input type="text" className="first" onChange={changeValue}/>
+              <span className={username ? "reg-username-span value-there" : "reg-username-span"}>Username</span>
             </div>
             <div className="register-email">
-              <input type="text" />
-              <span className="reg-email-span">Email</span>
+              <input type="text" className="second" onChange={changeValue}/>
+              <span className={email == true ? "reg-email-span value-there" : "reg-email-span"}>Email</span>
             </div>
             <div className="register-password">
-              <input type="password" />
-              <span className="reg-password-span value-there">Password</span>
+              <input type="password" className="third" onChange={changeValue}/>
+              <span className={password ? "reg-password-span value-there" : "reg-password-span"}>Password</span>
             </div>
             <div className="re-password">
-              <input type="text" />
-              <span className="repass-span">Repeat Password</span>
+              <input type="text" className="fourth" onChange={changeValue}/>
+              <span className={repass ? "repass-span value-there" : "repass-span"}>Repeat Password</span>
             </div>
             <div className="avatar">
               <label htmlFor="avatar">
