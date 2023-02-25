35846cbbf817447418e6d754f36b8480c057a1be
Erik Todorov <eriktdrv@gmail.com>
2023-02-25 23:43:18 +0200

    Adding copyright component

---
+++ client/src/components/Copyright/Copyright.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Copyright.css";
+
+export default function Copyright() {
+  return (
+    <div className="copyright">
+      <h1>© 2023 SalesIT. All rights reserved.</h1>
+    </div>
+  );
+}
