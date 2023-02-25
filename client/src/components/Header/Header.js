cda6df4174be7b744a8565086607d99a72339cfb
Erik Todorov <eriktdrv@gmail.com>
2023-02-25 22:20:19 +0200

    Adding link to nav

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,19 +1,22 @@
 import './Header.css';
-import React from 'react';
+import React from 'react'
+import { Link } from "react-router-dom";
 export default function Header(){
     return (
         <header className="navigation">
         <div className="mainlogo">
+            <Link to={'/'}>
             Sales<span className="itlogo">IT</span>
+            </Link>
         </div>
         <ul className="mainul">
-            <li><a>Home</a></li>
-            <li><a>Add Item</a></li>
-            <li><a>About</a></li>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/add-item'}>Add Item</Link></li>
+            <li><Link to={'/about'}>About</Link></li>
         </ul>
         <ul className="authul">
-            <li><a>Login</a></li>
-            <li><a>Register</a></li>
+            <li><Link to={'/login'}>Login</Link></li>
+            <li><Link to={'/register'}>Register</Link></li>
             <li><i className="fa-solid fa-user"></i></li>
         </ul>
     </header>   
