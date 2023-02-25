a5cc2052f7001af55a592a05ba0b1c4fcd3e3eed
Erik Todorov <eriktdrv@gmail.com>
2023-02-25 22:24:53 +0200

    Adding JSX & CSS to TopDiscounts

---
+++ client/src/components/Home/TopDiscounts/TopDiscounts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TopDiscounts.css";
+
+export default function TopDiscounts() {
+  return (
+    <div class="top-discounts">
+      <div class="title">
+        <h1>Top Discounts</h1>
+        <h4>Reccomended</h4>
+      </div>
+      <section class="discounts-section"></section>
+    </div>
+  );
+}
