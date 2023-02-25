d26b58857930ada39c893964c61517afbe1efd41
Erik Todorov <eriktdrv@gmail.com>
2023-02-25 18:49:34 +0200

    Using prettier

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,10 @@
-const router = require('express').Router();
-const authController = require('./controllers/authController')
-    router.get('/', (req, res) => {
-        console.log('React API working!')
-        res.end()
-    })
-    router.use(authController)
+const router = require("express").Router();
+const authController = require("./controllers/authController");
+
+router.get("/", (req, res) => {
+  console.log("React API working!");
+  res.end();
+});
+router.use(authController);
+
 module.exports = router;
