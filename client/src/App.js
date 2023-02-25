5795b77c00a6a4465570b91bb881302c6b0d0fbd
Erik Todorov <eriktdrv@gmail.com>
2023-02-25 22:54:45 +0200

    Adding home router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header/Header';
-
+import "./App.css";
+import Header from "./components/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
 function App() {
   return (
     <>
-    <Header/>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
