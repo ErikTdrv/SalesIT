351ff8ffe8044c4ddebe5502332588ce3f6974f3
Erik Todorov <eriktdrv@gmail.com>
2023-03-01 16:00:49 +0200

    Adding CSS to Add-Product

--- client/src/components/Products/Add-Products/Add-Product.js
+++ client/src/components/Products/Add-Products/Add-Product.js
@@ -1,8 +1,68 @@
-import React from "react";
-import './Add-Product.css'
+import React, { useState } from "react";
+import "./Add-Product.css";
 
-export default function AddProduct(){
-    return (
-        <h1>Form</h1>
-    )
+export default function AddProduct() {
+  let [formPage, setFormPage] = useState(0);
+  let [products, setProducts] = useState({});
+
+  function changeValue(e){
+      if(e.target.value.length > 0){
+       
+      }
+  }
+  return (
+    <div className="add-container">
+      <h1>Add Product</h1>
+      <div className="choosable">
+        <button className="all-btn">Phones</button>
+        <button className="computers-btn">Computers</button>
+        <button className="monitors-btn">Monitors</button>
+      </div>
+      <form className="add-form">
+        <div className="inputs">
+            <div className="left-input">
+            <div className="manufacturer">
+            <input type="text" onChange={changeValue} />
+            <span className={ products.manufacturer ? "manufacturer-span value-there" : "manufacturer-span"}>Manufacturer</span>
+            </div>
+          <div className="motherboard">
+            <input type="text" onChange={changeValue} />
+            <span className={ products.motherboard ? "motherboard-span value-there" : "motherboard-span"}>Motherboard</span>
+            </div>
+          <div className="processor">
+            <input type="text" onChange={changeValue} />
+            <span className={ products.processor ? "processor-span value-there" : "processor-span"}>Processor</span>
+          </div>
+          <div className="videocard">
+            <input type="text" onChange={changeValue} />
+            <span className={ products.videocard ? "videocard-span value-there" : "videocard-span"}>Videocard</span>
+          </div>
+            </div>
+            <div className="right-input">
+          <div className="os">
+            <input type="text" onChange={changeValue} />
+            <span className={ products.os ? "os-span value-there" : "os-span"}>Operation System</span>
+          </div>
+          <div className="ssd">
+            <input type="text" onChange={changeValue} />
+            <span className={ products.os ? "ssd-span value-there" : "ssd-span"}>SSD</span>
+          </div>
+          <div className="harddrive">
+            <input type="text" onChange={changeValue} />
+            <span className={ products.os ? "harddrive-span value-there" : "harddrive-span"}>Hard Drive</span>
+          </div>
+          <div className="price" id="price">
+            <input type="text" onChange={changeValue} />
+            <span className={ products.os ? "price-span value-there" : "price-span"}>Price</span>
+          </div>
+            </div>
+          </div>
+          <input
+              type="submit"
+              value="Add Product"
+              className="add-btn"
+            />
+      </form>
+    </div>
+  );
 }
