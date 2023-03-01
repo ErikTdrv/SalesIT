927b6272a3f394ac4df910e15fb4cf1d1ffb87e0
Erik Todorov <eriktdrv@gmail.com>
2023-03-01 17:37:43 +0200

    Styling ShoppingCard

--- client/src/components/Shopping/Shopping.js
+++ client/src/components/Shopping/Shopping.js
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
-import './Shopping.css'
+import "./Shopping.css";
 
-export default function ShoppingCard(){
-    return (
-        <div className="shopping-container">
-            
-        </div>
-    )
+export default function ShoppingCard() {
+  return (
+    <div className="shopping-container">
+      <h1>Shopping Card</h1>
+      <div className="card__products">
+        <h3>All Added Products</h3>
+        <section className="card__section">
+          <article className="card__product">
+            <img src="computer.jpg" alt="" />
+            <h1>Asus Veriton 32GB RAM 500GB SSD GeForce 3060 RTX</h1>
+            <span className="product__price">1200$</span>
+            <button>Remove</button>
+          </article>
+          <article className="card__product">
+            <img src="computer.jpg" alt="" />
+            <h1>Asus Veriton 32GB RAM 500GB SSD GeForce 3060 RTX</h1>
+            <span className="product__price">1200$</span>
+            <button>Remove</button>
+          </article><article className="card__product">
+            <img src="computer.jpg" alt="" />
+            <h1>Asus Veriton 32GB RAM 500GB SSD GeForce 3060 RTX</h1>
+            <span className="product__price">1200$</span>
+            <button>Remove</button>
+          </article>
+        </section>
+        <span className="total-price">Total Price: 12000$</span>
+      </div>
+      <button className="buy-btn">Buy</button>
+    </div>
+  );
 }
