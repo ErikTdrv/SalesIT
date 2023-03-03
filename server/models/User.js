663c42a40b3fcb584d40a3c919e1360545737557
Erik Todorov <eriktdrv@gmail.com>
2023-03-03 10:03:36 +0200

    Adding User model

---
+++ server/models/User.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: [4, "Username should have at least 4 characters!"],
+    maxlength: [10, "Username cannot have more than 10 characters!"],
+  },
+  email: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+    minlength: [6, "Password should have at least 6 characters!"],
+    maxlength: [12, "Password cannot have more than 12 characters!"],
+  },
+  avatarImg: {
+    required: false,
+    type: String,
+  },
+  imageId: {
+    required: false,
+    type: String,
+  },
+  phoneNumber: {
+    required: true,
+    type: String,
+  },
+  addedComputers: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Computer",
+    },
+  ],
+  addedMonitors: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Monitor",
+    },
+  ],
+  addedPhones: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Phone",
+    },
+  ],
+});
+
+userSchema.pre('save', function (next) {
+    bcrypt.hash(this.password, 10)
+        .then((hash) => {
+            this.password = hash
+            return next()
+        })
+})
+
+const User = new mongoose.model('User', userSchema)
+module.exports = User;
