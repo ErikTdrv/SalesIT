e5ef473ea5aaf75076ab04af60830993ef1482f5
Erik Todorov <eriktdrv@gmail.com>
2023-03-03 10:20:26 +0200

    Create a token service

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+const jwt = require('jsonwebtoken')
 const validateToken = (token) => {
     try {
         const data = jwt.verify(token, process.env.SECRET_KEY)
@@ -8,6 +8,23 @@ const validateToken = (token) => {
         throw new Error('Invalid cookie token!')
     }
 }
+const createAccessToken = (user) => {
+    const payload = {
+        _id: user._id,
+        email: user.email,
+        phone: user.phone,
+        username: user.username,
+    }
+    const accessToken = jwt.sign(payload, process.env.SECRET_KEY)
+    return {
+        email: user.email,
+        username: user.username,
+        phone: user.phone,
+        accessToken,
+        _id: user._id
+    };
+}
 module.exports = {
+    createAccessToken,
     validateToken
 }
