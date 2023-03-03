b269d9204433f7c7171841c0e979fad043b8106f
Erik Todorov <eriktdrv@gmail.com>
2023-03-03 10:23:48 +0200

    Adding register functionality

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 const validateToken = (token) => {
     try {
         const data = jwt.verify(token, process.env.SECRET_KEY)
@@ -24,7 +26,36 @@ const createAccessToken = (user) => {
         _id: user._id
     };
 }
+const register = async (data) => {
+    const existingEmail = await User.findOne({email: data.email})
+    const existingUsername = await User.findOne({username: data.username})
+
+    if(existingEmail){
+        throw new Error('Email already exists!')
+    }else if(existingUsername){
+        throw new Error('Username already exists!')
+    }
+    const user = await User.create(data)
+    return createAccessToken(user)
+}
+const login = async (email, password) => {
+    const user = await User.findOne({email});
+    if(!user){
+        throw new Error('Invalid email or password!')
+    }
+    const isUser = await bcrypt.compare(password, user.password)
+    if(isUser){
+        let userToReturn = await createAccessToken(user)
+        userToReturn.avatarImg = user.avatarImg;
+        userToReturn.imageId = user.imageId;
+        return userToReturn
+    }else {
+        throw new Error('Invalid email or password!')
+    }
+}
 module.exports = {
+    login,
+    register,
     createAccessToken,
     validateToken
 }
