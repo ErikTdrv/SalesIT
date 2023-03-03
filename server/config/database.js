c62bf26c0eb3e218a3317b7c0b45cbd5909fd24f
Erik Todorov <eriktdrv@gmail.com>
2023-03-03 12:07:34 +0200

    Configure database

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 function initDatabase(){
     mongoose.set('strictQuery', true)
-    return mongoose.connect(process.env.CONNECTIONSTRING);
+    return mongoose.connect('mongodb://0.0.0.0:27017/sales-it');
 }
 
 module.exports = initDatabase;
