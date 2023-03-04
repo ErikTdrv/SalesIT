4be89470aee288b4c87744a4516df637af8be8b2
Erik Todorov <eriktdrv@gmail.com>
2023-03-04 02:40:16 +0200

    Adding Login validation w/controlled forms

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -6,12 +6,32 @@ import "./Login.css";
 
 export default function Login() {
   const [authInfo, setAuthInfo] = useState({});
-  const [error, setError] = useState("");
+  const [error, setError] = useState({});
+  const [mainError, setMainError] = useState()
   async function loginHandler(e) {
     e.preventDefault();
     let data = await login(authInfo);
     if (data.message) {
-      setError(data.message);
+      setMainError(data.message);
+    }
+  }
+  function validateInput(e){
+    if(e.target.type == 'password'){
+      if(e.target.value.length < 4){
+        setError({...error, passwordErr: 'Password must contain minimum 6 characters!'})
+      }else if(e.target.value.length > 10){
+        setError({...error, passwordErr: 'Password must contain maximum 10 characters!'})
+      }else {
+        setError({...error, passwordErr: ''})
+      }
+    }else {
+      if(e.target.value.length < 4){
+        setError({...error, emailErr: 'Email must contain minimum 4 characters!'})
+      }else if(e.target.value.length > 12){
+        setError({...error, emailErr: 'Email must contain maximum 12 characters!'})
+      }else {
+        setError({...error, emailErr: ''})
+      }
     }
   }
   return (
@@ -19,6 +39,7 @@ export default function Login() {
       <div className="login">
         <div className="form">
           <h1>Login</h1>
+    {mainError ?  <p className="main-error">{mainError}</p> : ''}
           <form className="login-form" onSubmit={loginHandler}>
             <div className="inputs">
               <div className="email">
@@ -27,10 +48,12 @@ export default function Login() {
                   onChange={(e) =>
                     setAuthInfo({ ...authInfo, email: e.target.value })
                   }
+                  onBlur={validateInput}
                 />
                 <span className={authInfo.email ? "value-there" : ""}>
                   Email
                 </span>
+                {error.emailErr ? <p className="error">{error.emailErr}</p> : ''}
               </div>
               <div className="password">
                 <input
@@ -38,10 +61,12 @@ export default function Login() {
                   onChange={(e) =>
                     setAuthInfo({ ...authInfo, password: e.target.value })
                   }
+                  onBlur={validateInput}
                 />
                 <span className={authInfo.password ? "value-there" : ""}>
                   Password
                 </span>
+                {error.passwordErr ? <p className="error">{error.passwordErr}</p> : ''}
               </div>
             </div>
             <input
@@ -51,7 +76,6 @@ export default function Login() {
               className="login-btn"
             />
           </form>
-          {error && <p className="main-error">{error}</p>}
           <p className="text">
             Don't have an account? <Link to="/register">Sign Up</Link>
           </p>
