d1deed8de73587f019dbf43156b7798b79fa9560
Erik Todorov <eriktdrv@gmail.com>
2023-03-04 11:34:48 +0200

    Adding product controller to routes

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const authController = require("./controllers/authController");
-
+const productController = require("./controllers/productController")
 router.get("/", (req, res) => {
   console.log("React API working!");
   res.end();
 });
 router.use(authController);
-
+router.use(productController)
 module.exports = router;
