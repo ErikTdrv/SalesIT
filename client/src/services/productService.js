3bc929cad4641cb8a281d4b0b42736c8db2a9d60
Erik Todorov <eriktdrv@gmail.com>
2023-03-04 11:04:54 +0200

    Adding add product request

---
+++ client/src/services/productService.js
@@ -0,0 +1,18 @@
+const API_URL = "http://localhost:8080";
+
+export const addProduct = async (product) => {
+    try {
+        let request = await fetch(`${API_URL}/add-product`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify(product),
+        });
+        let data = await request.json();
+        console.log(data)
+    } catch (error) {
+        return error
+    }
+};
