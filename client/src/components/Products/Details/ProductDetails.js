f60ec782af41092e755233cf247f169cc2b96f13
Erik Todorov <eriktdrv@gmail.com>
2023-03-09 18:12:53 +0200

    Adjusting image switcher

--- client/src/components/Products/Details/ProductDetails.js
+++ client/src/components/Products/Details/ProductDetails.js
@@ -3,11 +3,11 @@ import "./ProductDetails.css";
 
 export default function ProductDetails() {
   let images = [
-    "computer1",
-    "computer2",
-    "computer3",
-    "computer4",
-    "computer5",
+    "/computer.jpg",
+    "/computer.jpg",
+    "/computer.jpg",
+    "/computer.jpg",
+    "/computer.jpg",
   ];
   let [index, setIndex] = useState(0);
   return (
@@ -15,13 +15,21 @@ export default function ProductDetails() {
       <div className="edit-image-div">
         <img src="/computer.jpg" alt="" className="details__image" />
         <div className="image__switcher">
-        <button onClick={() => setIndex(index - 1)}>&#60;</button>
+          <button onClick={() => setIndex(index - 1)}>&#60;</button>
           <section className="image__section">
-            <div className="div">{images[index]}</div>
-            <div className="div">{images[index + 1]}</div>
-            <div className="div">{images[index + 2]}</div>
+            <div className="div">
+              <img src={images[index]} alt="" />
+            </div>
+            <div className="div"> <img src={images[index + 1]} alt="" /></div>
+            <div className="div"> <img src={images[index + 2]} alt="" /></div>
           </section>
-        <button onClick={() => setIndex(index + 1)}>&#62;</button>
+          <button onClick={() => setIndex(index + 1)}>&#62;</button>
+        </div>
+        <div className="buttons">
+          <button>Back</button>
+          <button>Delete</button>
+          <button>Edit</button>
+          <button>Add Discount</button>
         </div>
       </div>
       <div className="info-div"></div>
