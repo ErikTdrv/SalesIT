417d838450c7b1f98de828c8b8031b0f22e7ec76
Erik Todorov <eriktdrv@gmail.com>
2023-03-12 12:20:21 +0200

    Adding isAuth validation for the header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,9 @@
 import './Header.css';
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from '../../contexts/AuthContext';
 export default function Header(){
+    let { isAuth } = useContext(AuthContext);
     return (
         <header className="navigation">
         <div className="mainlogo">
@@ -12,14 +14,23 @@ export default function Header(){
         <ul className="mainul">
             <li><NavLink activeclassname='active' className='Link' to='/'>Home</NavLink></li>
             <li><NavLink activeclassname='active' className='Link' to='/all-products'>All Products</NavLink></li>
+            { isAuth && 
             <li><NavLink activeclassname='active' className='Link' to='/add-product'>Add Product</NavLink></li>
+            }
             <li><NavLink activeclassname='active' className='Link' to='/about'>About</NavLink></li>
         </ul>
         <ul className="authul">
+            { isAuth ? 
+            <>
+            <li><i className="fa-solid fa-user"></i></li>
+            <li><NavLink activeclassname='activeCard' className='Link' to='/card'><i className="fa-solid fa-cart-shopping"></i></NavLink></li> 
+            </>
+            :
+            <>
             <li><NavLink activeclassname='active' className='Link' to='/login'>Login</NavLink></li>
             <li><NavLink activeclassname='active' className='Link' to='/register'>Register</NavLink></li>
-            <li><i className="fa-solid fa-user"></i></li>
-            <li><NavLink activeclassname='activeCard' className='Link' to='/card'><i className="fa-solid fa-cart-shopping"></i></NavLink></li>
+            </>
+            }
         </ul>
     </header>   
     );
