bd94760460fb833dc154dfd4f4d448969c323464
Erik Todorov <eriktdrv@gmail.com>
2023-03-15 23:19:47 +0200

    Adding validation

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -9,11 +9,14 @@ export default function Register() {
   const navigate = useNavigate()
   const [auth, setAuth] = useState({});
   const [mainError, setMainError] = useState()
+  const [error, setError] = useState({})
   let { userAuth, isAuth } = useContext(AuthContext);
   async function onSubmitHandler(e) {
     e.preventDefault();
+    if(auth.repass != auth.password){
+      return setMainError('Passwords must match!')
+    }
     let response = await register(auth);
-    console.log(response)
     if(response?.username){
       userAuth(response)
       navigate('/')
@@ -21,12 +24,11 @@ export default function Register() {
       setMainError(response.message)
     }
   }
-
   async function convertToBase64(file){
     return new Promise((resolve, reject) => {
       const fileReader = new FileReader();
       fileReader.readAsDataURL(file);
-
+      
       fileReader.onload = () => {
         resolve(fileReader.result)
       }
@@ -34,6 +36,11 @@ export default function Register() {
         reject(error)
       }
     })
+  }
+  function validateInput(type, e){
+    if(e.target.value === ''){
+      setError({...error, [type]: true})
+    }
   }
   return (
     <>
@@ -52,48 +59,59 @@ export default function Register() {
                 type="text"
                 className="first"
                 onChange={(e) => setAuth({ ...auth, username: e.target.value })}
+                onBlur={(e) => validateInput('username', e)}
               />
               <span className={auth.username ? "value-there" : ""}>
                 Username
               </span>
+              {error.username && <p className="error">Username is required!</p>}
             </div>
             <div className="register-email">
               <input
                 type="text"
                 className="second"
                 onChange={(e) => setAuth({ ...auth, email: e.target.value })}
+                onBlur={(e) => validateInput('email', e)}
               />
               <span className={auth.email ? "value-there" : ""}>Email</span>
+              {error.email && <p className="error">Email is required!</p>}
+
             </div>
             <div className="register-password">
               <input
                 type="password"
                 className="third"
                 onChange={(e) => setAuth({ ...auth, password: e.target.value })}
+                onBlur={(e) => validateInput('password', e)}
               />
               <span className={auth.password ? "value-there" : ""}>
                 Password
               </span>
+              {error.password && <p className="error">Password is required!</p>}
             </div>
             <div className="re-password">
               <input
                 type="password"
                 className="fourth"
                 onChange={(e) => setAuth({ ...auth, repass: e.target.value })}
+                onBlur={(e) => validateInput('repass', e)}
               />
               <span className={auth.repass ? "value-there" : ""}>
                 Repeat Password
               </span>
+              {error.repass && <p className="error">Re-Password is required!</p>}
             </div>
             <div className="phone">
               <input
                 type="text"
                 className="fifth"
                 onChange={(e) => setAuth({ ...auth, phone: e.target.value })}
+                onBlur={(e) => validateInput('phone', e)}
               />
               <span className={auth.phone ? "value-there" : ""}>
                 Phone Number
               </span>
+              {error.phone && <p className="error">Phone is required!</p>}
             </div>
             <div className="avatarDiv">
               <label htmlFor="avatar">
