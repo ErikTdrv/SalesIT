1c8f57121811d87bb87aaf4d3533cf6c63fdc318
Erik Todorov <eriktdrv@gmail.com>
2023-03-18 17:20:55 +0200

    Adjusting percentage component

--- client/src/components/Products/Details/Add-Discount/AddDiscount.js
+++ client/src/components/Products/Details/Add-Discount/AddDiscount.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "./AddDiscount.css";
+import { useParams, Link } from "react-router-dom";
 
-export default function AddDiscount(){
-    return (
-        <h1>Discount</h1>
-    )
+export default function AddDiscount() {
+  let { productId } = useParams();
+  const [discountPercentage, setDiscount] = useState(50);
+  const [error, setError] = useState("");
+
+  async function addDiscount() {
+    if (discountPercentage <= 0 || discountPercentage > 99) {
+      setError("Percentage have to be between 1% - 99% !");
+    } else {
+    }
+  }
+  return (
+    <div className="discount__container">
+      <h1>Add Discount</h1>
+      <div className="input__discount">
+        <input
+          type="number"
+          id="percentage"
+          name="percentage"
+          min="0"
+          max="100"
+          step="5"
+          value={discountPercentage}
+          onChange={(e) => setDiscount(e.target.value)}
+        />
+        <span>%</span>
+      </div>
+      <div className="buttons">
+      <Link to={`/all-products/${productId}`}>
+        <button>Back</button>
+      </Link>
+      <button onClick={addDiscount}>Add</button>
+      </div>
+      {error && <p className="discount__error">{error}</p>}
+    </div>
+  );
 }
