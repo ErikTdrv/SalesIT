964faddf746994299741c4cfc072af6427f21c3c
Erik Todorov <eriktdrv@gmail.com>
2023-03-21 16:32:32 +0200

    Adding loader

--- client/src/components/Shopping/Shopping.js
+++ client/src/components/Shopping/Shopping.js
@@ -1,26 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { getProductCard, removeProductFromCard } from "../../services/productService";
+import {
+  getProductCard,
+  removeProductFromCard,
+} from "../../services/productService";
 import "./Shopping.css";
 import { Link } from "react-router-dom";
 
 export default function ShoppingCard() {
   let [products, setProducts] = useState();
   let [isLoading, setIsLoading] = useState(true);
-  let [index, setIndex] = useState(0)
+  let [index, setIndex] = useState(0);
   let [totalPrice, setTotalPrice] = useState(0);
   useEffect(() => {
     async function getProducts() {
       let products = await getProductCard();
-      setProducts(products)
+      setProducts(products);
       setIsLoading(false);
-      setTotalPrice(products.reduce((acc, { price }) => Number(acc) + Number(price), 0))
+      setTotalPrice(
+        products.reduce((acc, { price }) => Number(acc) + Number(price), 0)
+      );
     }
     getProducts();
   }, []);
-  async function removeProduct(productId){
-    await removeProductFromCard(productId)
-    let updatedProducts = products.filter((product) => product._id !== productId)
-    setProducts(updatedProducts)
+  async function removeProduct(productId) {
+    await removeProductFromCard(productId);
+    let updatedProducts = products.filter(
+      (product) => product._id !== productId
+    );
+    setProducts(updatedProducts);
   }
   function validateIndex(type) {
     if (type === "+" && index + 4 < products.length) {
@@ -31,7 +38,6 @@ export default function ShoppingCard() {
   }
   return (
     <>
-    
       {!isLoading && products.length > 0 ? (
         <div className="shopping-container">
           <h1>Shopping Card</h1>
@@ -41,42 +47,76 @@ export default function ShoppingCard() {
               {products[index] && (
                 <article className="card__product">
                   <img src={products[index]?.images[0].imageUrl} alt="" />
-                  <h1>{products[index]?.manufacturer}{products[index]?.phonename}</h1>
-                  <span className="product__price">{products[index]?.price}$</span>
-                  <button onClick={() => removeProduct(products[index]._id)}>Remove</button>
-                    <Link to={`/all-products/${products[index]?._id}`}>
-                      <button>Details</button>
-                    </Link>
+                  <h1>
+                    {products[index]?.manufacturer}
+                    {products[index]?.phonename}
+                  </h1>
+                  <span className="product__price">
+                    {products[index]?.price}$
+                  </span>
+                  <button onClick={() => removeProduct(products[index]._id)}>
+                    Remove
+                  </button>
+                  <Link to={`/all-products/${products[index]?._id}`}>
+                    <button>Details</button>
+                  </Link>
                 </article>
               )}
-              { products[index + 1] && (
+              {products[index + 1] && (
                 <article className="card__product">
                   <img src={products[index + 1]?.images[0].imageUrl} alt="" />
-                  <h1>{products[index + 1]?.manufacturer}{products[index + 1]?.phonename}</h1>
-                  <span className="product__price">{products[index + 1]?.price}$</span>
-                  <button onClick={() => removeProduct(products[index + 1]._id)}>Remove</button>
-                    <Link to={`/all-products/${products[index + 1]?._id}`}>
-                      <button>Details</button>
-                    </Link>
+                  <h1>
+                    {products[index + 1]?.manufacturer}
+                    {products[index + 1]?.phonename}
+                  </h1>
+                  <span className="product__price">
+                    {products[index + 1]?.price}$
+                  </span>
+                  <button
+                    onClick={() => removeProduct(products[index + 1]._id)}
+                  >
+                    Remove
+                  </button>
+                  <Link to={`/all-products/${products[index + 1]?._id}`}>
+                    <button>Details</button>
+                  </Link>
                 </article>
               )}
-              { products[index + 2] && (
+              {products[index + 2] && (
                 <article className="card__product">
                   <img src={products[index + 2]?.images[0].imageUrl} alt="" />
-                  <h1>{products[index + 2]?.manufacturer}{products[index + 2]?.phonename}</h1>
-                  <span className="product__price">{products[index + 2]?.price}$</span>
-                  <button onClick={() => removeProduct(products[index + 2]._id)}>Remove</button>
+                  <h1>
+                    {products[index + 2]?.manufacturer}
+                    {products[index + 2]?.phonename}
+                  </h1>
+                  <span className="product__price">
+                    {products[index + 2]?.price}$
+                  </span>
+                  <button
+                    onClick={() => removeProduct(products[index + 2]._id)}
+                  >
+                    Remove
+                  </button>
                   <Link to={`/all-products/${products[index + 2]?._id}`}>
                     <button>Details</button>
                   </Link>
                 </article>
               )}
-              { products[index + 3] && (
+              {products[index + 3] && (
                 <article className="card__product">
                   <img src={products[index + 3]?.images[0].imageUrl} alt="" />
-                  <h1>{products[index + 3]?.manufacturer}{products[index + 3]?.phonename}</h1>
-                  <span className="product__price">{products[index + 3]?.price}$</span>
-                  <button onClick={() => removeProduct(products[index + 3]._id)}>Remove</button>
+                  <h1>
+                    {products[index + 3]?.manufacturer}
+                    {products[index + 3]?.phonename}
+                  </h1>
+                  <span className="product__price">
+                    {products[index + 3]?.price}$
+                  </span>
+                  <button
+                    onClick={() => removeProduct(products[index + 3]._id)}
+                  >
+                    Remove
+                  </button>
                   <Link to={`/all-products/${products[index + 3]?._id}`}>
                     <button>Details</button>
                   </Link>
@@ -84,8 +124,12 @@ export default function ShoppingCard() {
               )}
             </section>
             <div className="switch__buttons">
-              <button className="switchers" onClick={() => validateIndex('-')}>&#60;</button>
-              <button className="switchers" onClick={() => validateIndex('+')}>&#62;</button>
+              <button className="switchers" onClick={() => validateIndex("-")}>
+                &#60;
+              </button>
+              <button className="switchers" onClick={() => validateIndex("+")}>
+                &#62;
+              </button>
             </div>
             <span className="total-price">Total Price: {totalPrice}$</span>
           </div>
@@ -95,7 +139,11 @@ export default function ShoppingCard() {
         <div className="shopping-container">
           <h1>Shopping Card</h1>
           <div className="card__products">
-            <h3>No Added Products</h3>
+            {products?.length === 0 ? (
+              <h3>No Added Products</h3>
+            ) : (
+              <span class="loader"></span>
+            )}
           </div>
         </div>
       )}
