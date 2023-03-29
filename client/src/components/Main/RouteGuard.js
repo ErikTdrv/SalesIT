74f94067b0ac169c1b0bb519a390a60bb87e98f8
Erik Todorov <eriktdrv@gmail.com>
2023-03-29 10:23:33 +0300

    Creating user guard

--- client/src/components/Main/RouteGuard.js
+++ client/src/components/Main/RouteGuard.js
@@ -2,12 +2,20 @@ import { useContext } from "react";
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
-export const AuthGuard = ({children}) => {
-    const { isAuth } = useContext(AuthContext);
+export const AuthGuard = ({ children }) => {
+  const { isAuth } = useContext(AuthContext);
 
-    if(isAuth){
-        return <Navigate to='/' replace />;
-    }
+  if (isAuth) {
+    return <Navigate to="/" replace />;
+  }
 
+  return children;
+};
+export const UserGuard = ({ children }) => {
+  const { isAuth } = useContext(AuthContext);
+
+  if (isAuth) {
     return children;
-}
+  }
+  return <Navigate to="/login" replace />;
+};
