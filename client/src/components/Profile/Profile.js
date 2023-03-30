15428a8b6c214e8f6126deb718f117a57cd83a1f
Erik Todorov <eriktdrv@gmail.com>
2023-03-30 10:35:48 +0300

    Adding global user profile

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,40 +1,60 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
+import { getUserById } from "../../services/userService";
 import Copyright from "../Main/Copyright/Copyright";
 import ShoppingCard from "../Shopping/ShoppingCard";
 import "./Profile.css";
 
-export default function Profile() {
+export default function Profile({mode}) {
+  const [userInfo, setUserInfo] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
   const { user } = useContext(AuthContext);
 
+  const {userId} = useParams();
+  useEffect(() => {
+    if(mode === 'global'){
+      getUser()
+    }else {
+      setUserInfo(user)
+      setIsLoading(false)
+    }
+    async function getUser(){
+      setUserInfo(await getUserById(userId))
+      setIsLoading(false)
+    }
+  }, [])
   return (
     <>
+    {!isLoading ? (
       <div className="product__all">
         <div className="profile__info">
           <img
-            src={user.avatarImg}
+            src={userInfo.avatarImg}
             alt="avatar-img"
           />
           <div className="profile__info__auth">
             <li>
               Username:
-              <span> {user.username}</span>
+              <span> {userInfo.username}</span>
             </li>
             <li>
               Email:
-              <span> {user.email}</span>
+              <span> {userInfo.email}</span>
             </li>
             <li>
               Phone Number:
-              <span> {user.phone}</span>
+              <span> {userInfo.phone}</span>
             </li>
           </div>
         </div>
         <div className="profile__products">
-            <ShoppingCard mode={'profile'}/>
+          {mode === undefined && <ShoppingCard mode={'profile'}/>}
+          {mode === 'global' && <ShoppingCard mode={'global'} user={userInfo}/>}
         </div>
       </div>
-      <Copyright />
+    ) : <span className="loader"></span>}
+    <Copyright />
     </>
     
   );
