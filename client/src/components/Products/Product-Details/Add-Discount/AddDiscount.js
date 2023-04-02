01d43c783b2c6f793b553e8937b79fd79ca34785
Erik Todorov <eriktdrv@gmail.com>
2023-04-02 21:31:50 +0300

    Adding discount value when changing

--- client/src/components/Products/Product-Details/Add-Discount/AddDiscount.js
+++ client/src/components/Products/Product-Details/Add-Discount/AddDiscount.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./AddDiscount.css";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import { addDiscount, getOneProduct } from "../../../../services/productService";
+import {
+  addDiscount,
+  getOneProduct,
+} from "../../../../services/productService";
 
 export default function AddDiscount() {
   let { productId } = useParams();
-  const [discountPercentage, setDiscount] = useState(50);
-  const [productType, setProductType] = useState()
+  const [discountPercentage, setDiscount] = useState("");
+  const [productType, setProductType] = useState();
   const [error, setError] = useState("");
   const navigate = useNavigate();
   useEffect(() => {
-    async function getProduct(){
+    async function getProduct() {
       let data = await getOneProduct(productId);
+      setDiscount(data.discount);
       if (data.phonename) {
         setProductType("Phones");
       } else if (data.paneltype) {
@@ -20,39 +24,45 @@ export default function AddDiscount() {
         setProductType("Computers");
       }
     }
-    getProduct()
-  }, [])
+    getProduct();
+  }, []);
   async function onAddDiscount() {
     if (discountPercentage < 0 || discountPercentage > 99) {
       return setError("Discount have to be between 1% - 99% !");
     } else {
-      await addDiscount(discountPercentage, productId, productType)
+      await addDiscount(discountPercentage, productId, productType);
     }
-    navigate(`/all-products/${productId}`)
+    navigate(`/all-products/${productId}`);
   }
   return (
-    <div className="discount__container">
-      <h1>Add Discount</h1>
-      <div className="input__discount">
-        <input
-          type="number"
-          id="percentage"
-          name="percentage"
-          min="0"
-          max="100"
-          step="5"
-          value={discountPercentage}
-          onChange={(e) => setDiscount(e.target.value)}
-        />
-        <span>%</span>
-      </div>
-      <div className="buttons">
-      <Link to={`/all-products/${productId}`}>
-        <button>Back</button>
-      </Link>
-      <button onClick={onAddDiscount}>Add</button>
-      </div>
-      {error && <p className="discount__error">{error}</p>}
-    </div>
+    <>
+      {discountPercentage !== "" ? (
+        <div className="discount__container">
+          <h1>Add Discount</h1>
+          <div className="input__discount">
+            <input
+              type="number"
+              id="percentage"
+              name="percentage"
+              min="0"
+              max="100"
+              step="5"
+              value={discountPercentage}
+              onChange={(e) => setDiscount(e.target.value)}
+            />
+            <span>%</span>
+          </div>
+          <div className="buttons">
+            <Link to={`/all-products/${productId}`}>
+              <button>Back</button>
+            </Link>
+            <button onClick={onAddDiscount}>Add</button>
+          </div>
+          {error && <p className="discount__error">{error}</p>}
+        </div>
+      ) : (
+        <span className="loader"></span>
+      )}
+    </>
   );
 }
