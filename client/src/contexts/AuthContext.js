0af4efe61f7fb39403b73f0035662f99b37abf58
Erik Todorov <eriktdrv@gmail.com>
2023-04-03 23:01:07 +0300

    Update AuthContext.js

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,3 +1,5 @@
-import { createContext } from "react";
+// import { createContext } from "react";
+// export const AuthContext = createContext();
 
-export const AuthContext = createContext();
+
+//// - Not using it right now, switched to Redux
