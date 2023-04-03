268935674d55fb392800349f9a6bc277db6c4566
Erik Todorov <eriktdrv@gmail.com>
2023-04-03 17:08:46 +0300

    Creating reducer & store for Redux

---
+++ client/src/Redux/reducers.js
@@ -0,0 +1,21 @@
+const initialState = {
+  user: {},
+  isAuth: false,
+};
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.payload,
+        isAuth: Boolean(action.payload),
+      };
+    case "LOGOUT_USER":
+        return {
+            ...state, username: '', isAuth: false
+        }
+    default:
+      return state;
+  }
+};
