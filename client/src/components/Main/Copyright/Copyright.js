125367026e4278162c97cfd6da44ca85cc713869
Erik Todorov <eriktdrv@gmail.com>
2023-04-04 17:38:04 +0300

    Adding message to enable cookies

--- client/src/components/Main/Copyright/Copyright.js
+++ client/src/components/Main/Copyright/Copyright.js
@@ -2,8 +2,19 @@ import React from "react";
 import "./Copyright.css";
 
 export default function Copyright() {
+  const areCookiesEnabled = navigator.cookieEnabled;
+
   return (
     <div className="copyright">
+      {!areCookiesEnabled && (
+        <div className="cookie__access">
+          <p>
+            Please enable your third-party cookies from the Browser Settings to
+            get the full functionality of the application!
+          </p>
+        </div>
+      )}
+
       <h1>© 2023 SalesIT. All rights reserved.</h1>
     </div>
   );
