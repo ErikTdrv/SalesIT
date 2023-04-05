1256404d7f227e2a49e05c7e9cfce6a15bc751a0
Erik Todorov <eriktdrv@gmail.com>
2023-04-05 16:23:39 +0300

    Updating tests because of redux

---
+++ client/src/components/All-Products/All-Products.test.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import AllItems from "./All-Products";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "../../Redux/store";
+
+
+describe("All Products component", () => {
+  it("should render without crashing", () => {
+    render(
+      <Router>
+        <Provider store={store}>
+          <AllItems />
+        </Provider>
+      </Router>
+    );
+    const title = screen.getByTestId("loader");
+    expect(title).toBeInTheDocument();
+  });
+});
