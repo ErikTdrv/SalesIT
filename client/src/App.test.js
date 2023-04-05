a8cb188b04b87caa33bbd943ccf8a1c060868497
Erik Todorov <eriktdrv@gmail.com>
2023-04-05 16:23:31 +0300

    Updating tests because of Redux

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,14 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router} from 'react-router-dom';
-import App from './App';
-import { AuthContext } from './contexts/AuthContext';
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import store from "./Redux/store";
+import { Provider } from "react-redux";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(
     <Router>
+      <Provider store={store}>
         <App />
+      </Provider>
     </Router>
   );
-  const linkElement = screen.getByText('All Products');
+  const linkElement = screen.getByText("All Products");
   expect(linkElement).toBeInTheDocument();
 });
