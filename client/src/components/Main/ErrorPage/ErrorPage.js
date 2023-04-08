a19643acc60ec271aa75749731d48056f8d0e6b4
Erik Todorov <eriktdrv@gmail.com>
2023-04-08 15:03:57 +0300

    Adding button to home

--- client/src/components/Main/ErrorPage/ErrorPage.js
+++ client/src/components/Main/ErrorPage/ErrorPage.js
@@ -1,14 +1,16 @@
 import React from "react";
 import './ErrorPage.css';
 import Copyright from "../Copyright/Copyright";
+import { useNavigate } from "react-router-dom";
 
 export default function ErrorPage(){
+    const navigate = useNavigate();
     return (
         <>
         <div className="error__div">
             <p>404</p>
             <h1>Page Not Found</h1>
-            <button>Return To Home</button>
+            <button onClick={() => navigate('/')}>Return To Home</button>
         </div>
         <Copyright/>
         </>
