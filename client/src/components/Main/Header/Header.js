2cb171a7352b594ce9bc120009aa5a6dd555e37e
Erik Todorov <eriktdrv@gmail.com>
2023-04-11 19:34:52 +0300

    Adding responsive menu design

--- client/src/components/Main/Header/Header.js
+++ client/src/components/Main/Header/Header.js
@@ -1,48 +1,134 @@
-import './Header.css';
-import React, { useContext } from 'react'
+import "./Header.css";
+import React, { useContext, useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import { AuthContext } from '../../../contexts/AuthContext';
-import { useDispatch, useSelector } from 'react-redux';
-import { logoutUser } from '../../../services/userService';
-export default function Header(){
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const isAuth = useSelector(state => state.user.isAuth)
-    
-    async function userLogout(){
-        await logoutUser()
-        dispatch({ type: "LOGOUT_USER" });
-        navigate('/')
-    }
-    return (
-        <header className="navigation">
+import { AuthContext } from "../../../contexts/AuthContext";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "../../../services/userService";
+export default function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.user.isAuth);
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+  async function userLogout() {
+    await logoutUser();
+    dispatch({ type: "LOGOUT_USER" });
+    navigate("/");
+  }
+  return (
+    <>
+      <header className="navigation">
         <div className="mainlogo">
-            <Link className='Link' to='/'>
+          <Link className="Link" to="/">
             Sales<span className="itlogo">IT</span>
-            </Link>
+          </Link>
         </div>
-        <ul className="mainul">
-            <li><NavLink activeclassname='active' className='Link' to='/'>Home</NavLink></li>
-            <li><NavLink activeclassname='active' className='Link' to='/all-products'>All Products</NavLink></li>
-            { isAuth && 
-            <li><NavLink activeclassname='active' className='Link' to='/add-product'>Add Product</NavLink></li>
-            }
+        <ul className="mainul responsive-mainul">
+          <li>
+            <NavLink activeclassname="active" className="Link" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeclassname="active"
+              className="Link"
+              to="/all-products"
+            >
+              All Products
+            </NavLink>
+          </li>
+          {isAuth && (
+            <li>
+              <NavLink
+                activeclassname="active"
+                className="Link"
+                to="/add-product"
+              >
+                Add Product
+              </NavLink>
+            </li>
+          )}
         </ul>
         <ul className="authul">
-            { isAuth ? 
+          {isAuth ? (
             <>
-            <li><NavLink activeclassname='activeCard' className='Link' to='/profile'><i className="fa-solid fa-user"></i></NavLink></li>
-            <li><NavLink activeclassname='activeCard' className='Link' to='/card'><i className="fa-solid fa-cart-shopping"></i></NavLink></li> 
-            <li onClick={() => userLogout()} className='logout'>Logout</li>
+              <li>
+                <NavLink
+                  activeclassname="activeCard"
+                  className="Link"
+                  to="/profile"
+                >
+                  <i className="fa-solid fa-user"></i>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  activeclassname="activeCard"
+                  className="Link"
+                  to="/card"
+                >
+                  <i className="fa-solid fa-cart-shopping"></i>
+                </NavLink>
+              </li>
+              <li onClick={() => userLogout()} className="logout">
+                Logout
+              </li>
             </>
-            :
+          ) : (
             <>
-            <li><NavLink activeclassname='active' className='Link' to='/login'>Login</NavLink></li>
-            <li><NavLink activeclassname='active' className='Link' to='/register'>Register</NavLink></li>
+              <li>
+                <NavLink activeclassname="active" className="Link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  activeclassname="active"
+                  className="Link"
+                  to="/register"
+                >
+                  Register
+                </NavLink>
+              </li>
             </>
-            }
+          )}
         </ul>
-        <i class="fa-solid fa-bars bar"></i>
-    </header>   
-    );
+        <i
+          class="fa-solid fa-bars bar"
+          onClick={() => setIsMenuClicked(!isMenuClicked)}
+        ></i>
+      </header>
+      {isMenuClicked && (
+        <div className="more">
+          <ul className="mainul">
+            <li>
+              <NavLink activeclassname="active" className="Link" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                activeclassname="active"
+                className="Link"
+                to="/all-products"
+              >
+                All Products
+              </NavLink>
+            </li>
+            {isAuth && (
+              <li>
+                <NavLink
+                  activeclassname="active"
+                  className="Link"
+                  to="/add-product"
+                >
+                  Add Product
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
